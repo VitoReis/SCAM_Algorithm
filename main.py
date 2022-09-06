fadd602976fd3b851a5b5550fb5a582d2a842587
Vitor Silva Reis <vitorejuvian@hotmail.com>
2022-09-06 18:43:11 -0300

    Update

--- main.py
+++ main.py
@@ -2,29 +2,26 @@ from igraph import *
 import matplotlib.pyplot as plt
 
 def main():
-    # Lendo o grafo
-    graph = Graph.Read_Ncol("myGraph.ncol", directed=False)
+    graph = Graph.Read_Ncol("myGraph.ncol", directed=False)     # Lendo o grafo
     # plot(graph, target=plt.axes())
-    for vertice in graph.vs:              # Para cada vertice no grafo
-        if vertice['name'] == 's':        # Encontra o index de S e de T
-            s = vertice.index
-        elif vertice['name'] == 't':
-            t = vertice.index
 
-    deleted = []
-    # Mostrando o grafo inicial na tela
-    print('**************')
+    for vertex in graph.vs:              # Encontra o index de S e de T
+        if vertex['name'] == 's':
+            s = vertex.index
+        elif vertex['name'] == 't':
+            t = vertex.index
+
+    print('**************')     # Mostrando o grafo inicial na tela
     print('Initial graph:')
     print('**************')
     print(graph)
     print('**************\n\n')
     # plot(graph, layout='circle')
 
-    # Removendo os vertices de maior valor até o grafo se tornar não conexo
+    deleted = []
     connected = True
 
-    # Determina se existe um caminho de S a T
-    path = graph.get_all_simple_paths(s, t)
+    path = graph.get_all_simple_paths(s, t)     # Verifica se existe um caminho de S a T
     if len(path) >= 1:
         pathExist = True
     else:
@@ -37,58 +34,58 @@ def main():
         for v in graph.vs:
             print(f'Nome: {v["name"]} Index: {v.index}')
         print('***************')
-        verticeValue(graph)
-        deleteVertice(graph, deleted)
+        vertexValue(graph)
+        deleteVertex(graph, deleted)
         # plot(graph, target=plt.axes())
-        connected = verifyConnection(graph)
+        if graph.is_connected() == True:    # Verifica se o grafo é conexo
+            connected = True
+        else:
+            connected = False
 
-    # Mostrando o grafo final na tela
-    print('************')
+    print('************')       # Mostrando o grafo final na tela
     print('Final graph:')
     print('************')
     print(graph)
     print('************')
-    print(f'Deleted vertices: {deleted}')
+    print(f'Deleted vertex: {deleted}')
     print('************')
     # plot(graph, layout='circle')
 
-def verticeValue(graph):
+def vertexValue(graph):
     # Encontra o index de S e de T
-    for vertice in graph.vs:
-        if vertice['name'] == 's':
-            s = vertice.index
-        elif vertice['name'] == 't':
-            t = vertice.index
+    for vertex in graph.vs:
+        if vertex['name'] == 's':
+            s = vertex.index
+        elif vertex['name'] == 't':
+            t = vertex.index
 
-    # Determinando o valor dos vertices
+    # Determina o valor dos vertices
     paths = graph.get_all_simple_paths(s, t)
     print(f'Paths by value: {paths}')
     graph.vs['value'] = 0
     for path in paths:    # Para cada caminho existente, se o vertice não é 'S' ou 'T', e esta em algum caminho...
-        for vertice in path:
-            if graph.vs[vertice]['name'] != 's' and graph.vs[vertice]['name'] != 't':
-                graph.vs[vertice]['value'] += 1                                     # ...adiciona 1 ao valor do vertice
+        for vertex in path:
+            if graph.vs[vertex]['name'] != 's' and graph.vs[vertex]['name'] != 't':
+                graph.vs[vertex]['value'] += 1                                     # ...adiciona 1 ao valor do vertice
     print(f'Vertice values: {graph.vs["value"]}')
 
-def deleteVertice(graph, deleted):
+def deleteVertex(graph, deleted):
+    # Encontra o vertice de maior valor
     greater = -1
     greaterIndex = -1
-    for vertice in graph.vs:                                # Para cada vertice no grafo
-        if vertice['name'] != 's' and vertice['name'] != 't':   # Se o vertise não é o S e não é o T
-            if vertice['value'] > greater:                    # Se a particopação dele é a maior
-                greaterName = vertice['name']               # Define o nome do vertice de maior valor
-                greater = vertice['value']                    # Define o maior como esse vertice
-                greaterIndex = vertice.index                      # Define o index do maior como o desse vertice
-    print(f'Deleting vertice: {greaterName}')
+    for vertex in graph.vs:
+        if vertex['name'] != 's' and vertex['name'] != 't':
+            if vertex['value'] > greater:
+                greaterName = vertex['name']
+                greater = vertex['value']
+                greaterIndex = vertex.index
+    print(f'Deleting vertex: {greaterName}')
     print('***************\n\n')
-    deleted.append(greaterName)
-    graph.delete_vertices(graph.vs(greaterIndex))                 # Deleta o vertice de maior tamanho passando o index dele
 
-def verifyConnection(graph):
-    for vertice in graph.vs:
-        if graph.is_connected(vertice.index) == False:
-            return False
-    return True
+    # Salva o nome dos vertices deletados em uma lista para melhor visualização posteriormente
+    deleted.append(greaterName)
+    # Deleta o vertice de maior valor
+    graph.delete_vertices(graph.vs(greaterIndex))
 
 if __name__ == '__main__':
     main()
